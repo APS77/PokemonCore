0cd684b9aa0166d82b2470dc836c12c3ffb81410
MartiBrgs <martinbrgs@gmail.com>
2021-09-11 18:45:24 -0300

    update canvas 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-let canvas = document.getElementById("canvas")
+let canvas = document.getElementById("canvas");
 
-canvas.style.background = "#784212"
+let window_height = window.innerHeight;
+let window_width = window.innerWidth;
+
+canvas.height = window_height;
+canvas.width = window_width;
+
+canvas.style.background = "#784212";
