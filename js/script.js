9a5d069e6c4e275c4b5b0a2dc574a6788dc75147
MartiBrgs <martinbrgs@gmail.com>
2021-09-15 19:51:06 -0300

    añadido funcion para detectar colision jugador pasto

--- js/script.js
+++ js/script.js
@@ -5,29 +5,28 @@ let window_height = window.innerHeight;
 let window_width = window.innerWidth;
 
 let fps, fpsInterval, startTime, now, then, elapsed;
+let keys = [];
 
 canvas.height = 400;
 canvas.width = 760;
 
 // Imagenes
-const playerSprite = new Image();
-playerSprite.src = "img/player2.png";
+const playerSprite = new Image(); // 128 x 192 ideal
+playerSprite.src = "img/p6.png";
 const imagenFondo = new Image();
 imagenFondo.src = "img/fondo.png";
 const pasto = new Image();
 pasto.src = "img/pasto.png";
 
-let keys = [];
-
 // Player
 const player = {
-    posX: 14,
-    posY: 55,
-    width: 64,
-    height: 64,
+    posX: 3,
+    posY: 65,
+    width: 32,
+    height: 48,
     frameX: 0,
     frameY: 0,
-    speed: 2.5,
+    speed: 5,
     moving: false
 }
 
@@ -66,17 +65,21 @@ function movePlayer () {  //  A D W S (en ese orden)
 
     if (keys[0] == 65 && keys.length == 1 || keys[0] == 68 && keys[1] == 65 || keys[0] == 65 && keys[1] == 68) {
         player.frameY = 1;
-        if (player.posX > -6) {
+        if (player.posX > 0) {
             player.posX -= player.speed;
         }
     }
     if (keys[0] == 68 && keys.length == 1) {
         player.frameY = 2;
-        if (player.posX < canvas.width - player.width/2.1) {
+        if (player.posX < canvas.width - player.width/1.4) {
             player.posX += player.speed;
         }
     }
-    if (keys[keys.length - 1] == 87 || keys[0] == 87 || keys[0] == 83 && keys[1] == 87 || keys[0] == 87 && keys[1] == 83) {
+    if (keys[keys.length - 1] == 87    ||
+        keys[0] == 87                  || 
+        keys[0] == 83 && keys[1] == 87 ||
+        keys[0] == 87 && keys[1] == 83
+        ){
         player.frameY = 3;
         if (player.posY > 28) {
             player.posY -= player.speed;
@@ -84,7 +87,7 @@ function movePlayer () {  //  A D W S (en ese orden)
     }
     if (keys[keys.length - 1] == 83 && keys[0] != 87 || keys[0] == 83 && keys[1] != 87) {
         player.frameY = 0;
-        if (player.posY < canvas.height - player.height/1.8) {
+        if (player.posY < canvas.height - player.height + 10) {
             player.posY += player.speed;
         }
     }
@@ -94,8 +97,6 @@ function handlePlayerFrame () {
     (player.frameX < 3 && player.moving) ? 
     player.frameX++ : player.frameX = 0;
 }
-
-
 
 function startAnimating (fps) {
     fpsInterval = 1000/fps;
@@ -111,55 +112,89 @@ function animate () {
     if (elapsed > fpsInterval) {
         then = now - (elapsed % fpsInterval);
         context.drawImage(imagenFondo, 0, 0, canvas.width, canvas.height);
-        creaPasto(20, 200, 6, 7);
+        creaPasto(30, 200, 6, 7);
         creaPasto(260, 80, 9, 5);
         creaPasto(540, 280, 9, 3);
-        creaPasto(600, 55, 6, 3);
+        creaPasto(600, 60, 6, 3);
+        colissionDetect(rectPlayer(player.posX, player.posY, player.width, player.height), rectPasto1, rectPasto2, rectPasto3, rectPasto4);
+        showColissionHitbox(rectPlayer(player.posX, player.posY, player.width, player.height), rectPasto1, rectPasto2, rectPasto3, rectPasto4);
+        //showGrassHitbox(rectPasto1, rectPasto2, rectPasto3, rectPasto4);
         drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY,
-        player.width, player.height, player.posX, player.posY, player.width/1.8, player.height/1.8);
+        player.width, player.height, player.posX, player.posY, player.width/1.3, player.height/1.3);
         requestAnimationFrame(animate);
         movePlayer();
         handlePlayerFrame();  
-        //rectPlayer(player.posX, player.posY, player.width, player.height);  
-        // SOLUCIONAR CONSUME MUCHA MEMORIA
-        colissionDetect(rectPlayer(player.posX, player.posY), rectPasto1);
-        colissionDetect(rectPlayer(player.posX, player.posY), rectPasto2);
     }
 }
 
-function rectPlayer(posX, posY) {
-    return [posX, posY, posX + 20, posY + 20];
+function rectPlayer(posX, posY, width, height) {
+    return [posX + 2, posY + 5, width - 12, height - 18]; // hitbox Player
 }
-// Pasto y sus funcionalidades
 
-// Funcion que da info de los pixeles que ocupa cada seccion de pasto (manual):
-// Con esta info se le puede dar funcionalidad despues
 function infoPasto (posX, posY, nHorizontal, nVertical) {
-    //console.log(`Seccion de pasto iniciada en x: ${posX} y: ${posY}`)
-    //console.log("Pos Inicial X: " + posX + " | Pos Final X: " + (posX + nHorizontal * 20))
-    //console.log("Pos Inicial Y: " + posY + " | Pos Final Y: " + (posY + nVertical * 20))
-    return [posX , posY - 10, posX + 5 + (nHorizontal - 1) * 20, posY + nVertical * 20 - 10];
+    return [posX , posY, nHorizontal * 20, nVertical * 20];
 }
 
-let rectPasto1 = infoPasto(20, 200, 6, 7);
+let rectPasto1 = infoPasto(30, 200, 6, 7);
 let rectPasto2 = infoPasto(260, 80, 9, 5);
 let rectPasto3 = infoPasto(540, 280, 9, 3);
-let rectPasto4 = infoPasto(600, 55, 6, 3);
+let rectPasto4 = infoPasto(600, 60, 6, 3);
 
-function colissionDetect(rectPlayer, rectPasto) {
-    let p = rectPasto,
-        j = rectPlayer;
-    if (j[0] > p[2] ||
-        j[2] < p[0] ||
-        j[3] < p[1] ||
-        j[3] > p[3]
-    ) {
-        // no colission
-        console.log(p);
-        console.log(j);
-    } else {
-        console.log("Colision detectada");
-    }
+function colissionDetect(rectPlayer, ...rectPasto) {
+    rectPasto.forEach (function (pasto) {
+        if (rectPlayer[0] >= pasto[2] + pasto[0]  ||
+            rectPlayer[2] + rectPlayer[0] <= pasto[0]  ||
+            rectPlayer[3] + rectPlayer[1] <= pasto[1] ||
+            rectPlayer[3] + rectPlayer[1] >= pasto[3] + pasto[1] + 10
+        ) {
+            // no colission
+        } else {
+            // colission
+            console.log("Colision detectada");
+            // HACER ALGO AQUI
+        }
+    } )
+}
+
+function showPlayerHitbox (rectPlayer) {
+    // show player's hitbox
+    context.rect(rectPlayer[0], rectPlayer[1], rectPlayer[2], rectPlayer[3])
+    context.stroke(); 
+}
+
+function showGrassHitbox (...rectPasto) {
+    rectPasto.forEach ( function (pasto) {
+        context.beginPath();
+        context.rect(pasto[0], pasto[1], pasto[2], pasto[3]);
+        context.strokeStyle = "black";
+        context.stroke(); 
+    } )
+}
+
+function showColissionHitbox (rectPlayer, ...rectPasto) {
+    // show player's hitbox
+    context.rect(rectPlayer[0], rectPlayer[1], rectPlayer[2], rectPlayer[3])
+    context.stroke(); 
+
+    rectPasto.forEach (function (pasto) {
+        if (rectPlayer[0] >= pasto[2] + pasto[0]  - 5||
+            rectPlayer[2] + rectPlayer[0] <= pasto[0] + 5 ||
+            rectPlayer[3] + rectPlayer[1] <= pasto[1] ||
+            rectPlayer[3] + rectPlayer[1] >= pasto[3] + pasto[1] + 10
+        ) {
+            // no colission
+            context.beginPath();
+            context.rect(pasto[0], pasto[1], pasto[2], pasto[3]);
+            context.strokeStyle = "black";
+            context.stroke(); 
+        } else {
+            // colission
+            context.beginPath();
+            context.rect(pasto[0], pasto[1], pasto[2], pasto[3]);
+            context.strokeStyle = "red";
+            context.stroke();
+        }
+    } )
 }
 
 // Funcion que crea seccion de pasto como matriz rectangular
@@ -171,11 +206,11 @@ function creaPasto (posX, posY, nHorizontal, nVertical) {
     }
 }
 
-startAnimating(100);
+startAnimating(24);
 
 
-/*function move (e) {
-    // var keyCode = (window.event) ? e.which : e.keyCode;
-    // alert(keyCode);
+function getKeyCode (e) {
+    var keyCode = (window.event) ? e.which : e.keyCode;
+    console.log(keyCode);
 }
-document.onkeydown = move; */
+//windows.addEventListener("keydown", getKeyCode);
