8be758a1d12156688333627675702a711a5f1273
MartiBrgs <martinbrgs@gmail.com>
2021-09-25 15:43:40 -0300

    update fondos

--- js/battle.js
+++ js/battle.js
@@ -1,10 +1,7 @@
-//let canvasBattle = document.getElementById("battle");
-//let context = canvas.getContext("2d");
-
-//canvasBattle.height = 400;
-//canvasBattle.width = 760;
-
-const battlePathBG = "img/battle/battle3.png";
+//export const battleBG = new Image();
+//battleBG.src = "img/battle/battle3.png";
+const imagenFondo = new Image();
+let battlePathBG = "img/battle/battle3.png";
 export const pkmn1 = new Image();
 pkmn1.src = "img/pokemons/back/b_bw_162.png"
 export const pkmn2 = new Image();
@@ -12,8 +9,8 @@ pkmn2.src = "img/pokemons/front/spr_bw_157.png"
 
 export function createBattle (ctx, cvs) {
     //player.deactivate();
-    // imagenFondo.src = battlePathBG;
-    ctx.drawImage(battleBG, 0, 0, cvs.width, cvs.height);
+    imagenFondo.src = battlePathBG;
+    ctx.drawImage(imagenFondo, 0, 0, cvs.width, cvs.height);
     ctx.drawImage(pkmn1, 120, 130, 250, 250);
     ctx.drawImage(pkmn2, 420, 60, 180, 180);
 }
