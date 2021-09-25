756d5618999af51284b85527f7bf776f8ee509e2
MartiBrgs <martinbrgs@gmail.com>
2021-09-25 15:43:10 -0300

    sacar funciones de aqui para añadirlas a la clase battlttile

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ const config = {
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
+
 // let window_height = window.innerHeight;
 // let window_width = window.innerWidth;
 let fpsInterval, startTime, now, then, elapsed;
@@ -32,7 +33,7 @@ let grass = new BattleTile (pasto);
 grass.setTilePosX = 30;
 grass.setTilePosY = 200;
 grass.setTileRatio = 10;
-console.log(grass.getHitbox());
+grass.setTileRatio = 1;
 
 // Player Instance
 let juan = new Player (playerSprite);
@@ -56,18 +57,12 @@ function animate () {
     if (elapsed > fpsInterval) {
         then = now - (elapsed % fpsInterval);
         context.drawImage(imagenFondo, 0, 0, canvas.width, canvas.height);
-        //creaPasto(30, 200, 6, 7);
-        //creaPasto(260, 80, 9, 5);
-        //creaPasto(540, 280, 9, 3);
-        //creaPasto(600, 60, 6, 3);
         grass.drawTile(context);
         juan.drawSprite(context);
-        //showColissionHitbox(juan.getHitbox(), rectPasto1, rectPasto2, rectPasto3, rectPasto4);
-        //colissionDetect(juan.getHitbox(), rectPasto1, rectPasto2, rectPasto3, rectPasto4);
-        
-        //showGrassHitbox(rectPasto1, rectPasto2, rectPasto3, rectPasto4);
-        juan.showPlayerHitbox(context);
         grass.showTileHitbox(context);
+        juan.showPlayerHitbox(context);
+        collisionDetect( juan.getHitbox(), grass.getHitbox() );
+        //grass.battleLauncher();
         requestAnimationFrame(animate);
         juan.movePlayer();
         juan.handlePlayerFrame();
@@ -76,81 +71,22 @@ function animate () {
 
 startAnimating(24);
 
-function infoPasto (posX, posY, nHorizontal, nVertical) {
-    return [posX , posY, nHorizontal * 20, nVertical * 20];
+function isPlayerOffTheTile (playerHitbox, battleTile) {
+    return playerHitbox[0] >= battleTile[2] + battleTile[0] - 5 ||
+           playerHitbox[2] + playerHitbox[0] <= battleTile[0] + 5 ||
+           playerHitbox[3] + playerHitbox[1] <= battleTile[1] ||
+           playerHitbox[3] + playerHitbox[1] >= battleTile[3] + battleTile[1] + 10
 }
 
-let rectPasto1 = infoPasto(30, 200, 6, 7);
-let rectPasto2 = infoPasto(260, 80, 9, 5);
-let rectPasto3 = infoPasto(540, 280, 9, 3);
-let rectPasto4 = infoPasto(600, 60, 6, 3);
-
-function colissionDetect(getHitbox, ...rectPasto) {
-    rectPasto.forEach (function (pasto) {
-        if (getHitbox[0] >= pasto[2] + pasto[0] - 5 ||
-            getHitbox[2] + getHitbox[0] <= pasto[0] + 5 ||
-            getHitbox[3] + getHitbox[1] <= pasto[1] ||
-            getHitbox[3] + getHitbox[1] >= pasto[3] + pasto[1] + 10
-        ) {
-            // no colission
-        } else {
-            // colission
-            console.log("Colision detectada");
-            //createBattle(context, canvas);
-        }
-    } )
-}
-
-function showGrassHitbox (...rectPasto) {
-    rectPasto.forEach ( function (pasto) {
-        context.beginPath();
-        context.rect(pasto[0], pasto[1], pasto[2], pasto[3]);
-        context.strokeStyle = "black";
-        context.stroke(); 
-    } )
-}
-
-function showColissionHitbox (getHitbox, ...rectPasto) {
-    // show player's hitbox
-    context.rect(getHitbox[0], getHitbox[1], getHitbox[2], getHitbox[3])
-    context.stroke(); 
-
-    rectPasto.forEach (function (pasto) {
-        if (getHitbox[0] >= pasto[2] + pasto[0]  - 5 ||
-            getHitbox[2] + getHitbox[0] <= pasto[0] + 5 ||
-            getHitbox[3] + getHitbox[1] <= pasto[1] ||
-            getHitbox[3] + getHitbox[1] >= pasto[3] + pasto[1] + 10
-        ) {
-            // no colission
-            context.beginPath();
-            context.rect(pasto[0], pasto[1], pasto[2], pasto[3]);
-            context.strokeStyle = "black";
-            context.stroke(); 
-        } else {
-            // colission
-            context.beginPath();
-            context.rect(pasto[0], pasto[1], pasto[2], pasto[3]);
-            context.strokeStyle = "red";
-            context.stroke();
-        }
+function collisionDetect(playerHitbox, ...battleTileHitbox) {
+    resetHitboxColor();
+    battleTileHitbox.forEach (function (battleTile) {
+        grass.playerOnTile = false;
+        if ( isPlayerOffTheTile(playerHitbox, battleTile) ) return;
+        grass.playerOnTile = true;
+        changeHitboxColor();
     })
 }
 
-// Funcion que crea seccion de pasto como matriz rectangular
-function creaPasto (posX, posY, nHorizontal, nVertical) {
-    for (let x = posX; x < posX + nHorizontal * 20; x += 20) {
-        for (let y = posY; y < posY + nVertical * 20; y += 20) {
-            context.drawImage(pasto, x, y, 20, 20);
-        }
-    }
-}
-
-
-
-/*
-function getKeyCode (e) {
-    var keyCode = (window.event) ? e.which : e.keyCode;
-    console.log(keyCode);
-}
-//windows.addEventListener("keydown", getKeyCode);
-*/
+function resetHitboxColor () {context.strokeStyle = "black";}
+function changeHitboxColor () {context.strokeStyle = "red";}
