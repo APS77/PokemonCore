ce8bcfb1a6e1b95237e013f01a6d19740138a955
MartiBrgs <martinbrgs@gmail.com>
2021-09-26 12:08:59 -0300

    Update random encounter

--- js/BattleTile_class.js
+++ js/BattleTile_class.js
@@ -22,7 +22,10 @@ export class BattleTile {
         this.ratio = ratio;
     }
 
-    drawTile (context) {
+    drawTile (context, posx, posy, ratio) {
+        this.setTilePosX = posx;
+        this.setTilePosY = posy;
+        this.setTileRatio = ratio;
         context.drawImage(this.tileIMG, this.posX, this.posY, this.width, this.height);
     }
 
@@ -40,35 +43,34 @@ export class BattleTile {
         return Math.random() * 100;
       }
 
-    randomBattleCalculator () {
+    battleChecker () {
+        if ( this.battleIndicator[0] >= this.battleIndicator[1] ) return true;
+    }
+
+    randomBattleHandler () {
         let randomNumber = this.getRandom();
-        this.battleIndicator = [];
-        if (this.ratio >= randomNumber) {
-            this.battleIndicator.push(this.ratio);
-            this.battleIndicator.push(randomNumber);
-            console.log(this.battleIndicator);
-        } 
+        if (this.battleIndicator.length >= 2) return;
+        this.battleIndicator.push(this.ratio);
+        this.battleIndicator.push(randomNumber);
+        console.log(this.battleIndicator);
+        return this.battleChecker();
     }
 
     isbattle () {
         if ( !this.playerOnTile ) return;
-        this.randomBattleCalculator ();
+        return this.randomBattleHandler();
     }
 
     battleLauncher () {
-        this.isbattle();
-        if ( this.battleIndicator[0] >= this.battleIndicator[1] ) {
-            console.log("BATALLA INICIADA");
-        }
+        if ( !this.isbattle() ) return;
+        console.log("BATALLA INICIADA");
     }
 
-    /*
-    insertTiles (posX, posY, rows, columns) {
+    drawTiles (context, posX, posY, rows, columns) {
         for (let x = posX; x < posX + rows * 20; x += 20) {
             for (let y = posY; y < posY + columns * 20; y += 20) {
                 context.drawImage(this.tileIMG, x, y, 20, 20);
             }
         }
     }
-    */
 }
