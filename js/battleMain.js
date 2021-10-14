8d368905e072269eaa0b2a94cdd378931db85fcd
MartiBrgs <martinbrgs@gmail.com>
2021-10-14 18:05:44 -0300

    created wild pokemon damage dealer

--- js/battleMain.js
+++ js/battleMain.js
@@ -16,11 +16,15 @@ export let inBattle = false;
 
 // BATTLE MAIN ------------------------------------------------------------------------------------------
 
-// Attacks
+// Furret Attacks
 Pokemon.furret.attacks.push(Attack.scratch);
 Pokemon.furret.attacks.push(Attack.slam);
 Pokemon.furret.attacks.push(Attack.tackle);
 Pokemon.furret.attacks.push(Attack.fire_punch);
+
+// Bayleef Attacks
+Pokemon.bayleef.attacks.push(Attack.tackle);
+Pokemon.bayleef.attacks.push(Attack.razor_leaf);
 
 // USEFUL FUNCTIONS FOR HTML
 function clearBox(elementID) {
@@ -83,6 +87,11 @@ function drawBattle(pkmn1Sprites, pkmn2Sprites) {
 function drawBattleWin(pkmn1Sprites) {
     context.drawImage(battleBG, 0, 0, canvas.width, canvas.height);
     context.drawImage(pkmn1Sprites.back, 110, 130, 250, 250);
+}
+
+function drawBattleLose(pkmn2Sprites) {
+    context.drawImage(battleBG, 0, 0, canvas.width, canvas.height);
+    context.drawImage(pkmn2Sprites.front, 410, 70, 200, 200);
 }
 /*
 ############
@@ -195,44 +204,69 @@ function showPokemonAttacks(player) {
     let sect = createSection("attacks","attackButtons");
     document.getElementById("attackMenu").appendChild(sect);
     for (let i = 0; i < 4; i++) {
-        showAttackBtn(i, player);
+        showAttackBtn(i, player, player.pokemons[0]);
     }
 }
-function showAttackBtn(i, player) {
-    let btn = createButton(player.pokemons[0].attacks[i].name);
+function showAttackBtn(i, player, playerPokemon) {
+    let btn = createButton(playerPokemon.attacks[i].name);
     btn.addEventListener("click", function () {
-        pushTextLine(`${player.pokemons[0].name} used ${player.pokemons[0].attacks[i].name}!`);
-        damageDealer(i, player)
-        battleChecker(player);
+        pushTextLine(`${playerPokemon.name} used ${playerPokemon.attacks[i].name}!`);
+        damageDealer(i, player, playerPokemon, Pokemon.bayleef);
+        enemyPokemonDamageDealer(player, playerPokemon, Pokemon.bayleef);
+        battleChecker(player, playerPokemon, Pokemon.bayleef);
     });
     document.getElementById("attackButtons").appendChild(btn);
 }
 
-function damageDealer(i, player) {
+function damageDealer(i, player, playerPokemon, enemyPokemon) {
     clearBox("battleHeader");
-    if (Pokemon.bayleef.currentHP - player.pokemons[0].attacks[i].power < 0) {
-        pushTextLine(`The attack did ${Pokemon.bayleef.currentHP} damage`);
-        Pokemon.bayleef.currentHP = 0;
-    } else {
-        Pokemon.bayleef.currentHP -= player.pokemons[0].attacks[i].power;
-        pushTextLine(`The attack did ${player.pokemons[0].attacks[i].power} damage`);
-    }
+    damageCalc(i, playerPokemon, enemyPokemon);
     battleHeader(player);
 }
 
-function battleChecker(player) {
-    if (Pokemon.bayleef.currentHP == 0) {
-        pushTextLine("foe " + Pokemon.bayleef.name + " fainted!");
-        pushTextLine("You won the battle!");
-        drawBattleWin(player.pokemons[0].sprites);
-        setTimeout(function() {endBattle(player);}, 2000);
-        console.log("Battle Ended!!");
+function damageCalc(i, pokemon1, pokemon2) {
+    if (pokemon2.currentHP - pokemon1.attacks[i].power < 0) {
+        pushTextLine(`The attack did ${pokemon2.currentHP} damage`);
+        pokemon2.currentHP = 0;
+    } else {
+        pokemon2.currentHP -= pokemon1.attacks[i].power;
+        pushTextLine(`The attack did ${pokemon1.attacks[i].power} damage`);
     }
 }
 
-function endBattle (player) {
+function getRandomAttackId(enemyPokemon) {
+    let randomAttackId = Math.floor(Math.random() * enemyPokemon.attacks.length);
+    return randomAttackId;
+}
+
+function enemyPokemonDamageDealer(player, playerPokemon, enemyPokemon) {
+    clearBox("battleHeader");
+    let randomIndex = getRandomAttackId(enemyPokemon);
+    console.log(enemyPokemon.attacks[randomIndex]);
+    pushTextLine(`${enemyPokemon.name} used ${enemyPokemon.attacks[randomIndex].name}!`);
+    damageCalc(randomIndex, enemyPokemon, playerPokemon);
+    battleHeader(player);
+}
+
+function battleChecker(player, playerPokemon, enemyPokemon) {
+    if (enemyPokemon.currentHP == 0) {
+        pushTextLine("foe " + enemyPokemon.name + " fainted!");
+        pushTextLine("You won the battle!");
+        drawBattleWin(playerPokemon.sprites);
+        setTimeout(function() {endBattle(player, playerPokemon, enemyPokemon);}, 2000);
+        console.log("Battle Ended!!");
+    } else if (playerPokemon.currentHP == 0) {
+        pushTextLine(playerPokemon.name + " fainted!");
+        pushTextLine("You lose the battle!");
+        drawBattleLose(enemyPokemon.sprites);
+        setTimeout(function() {endBattle(player, playerPokemon, enemyPokemon);}, 2000);
+    }
+}
+
+function endBattle (player, playerPokemon, enemyPokemon) {
     clearBattleHTML();
-    Pokemon.bayleef.currentHP = Pokemon.bayleef.baseStats.HP;
+    playerPokemon.currentHP = playerPokemon.baseStats.HP;
+    enemyPokemon.currentHP = enemyPokemon.baseStats.HP;
     inBattle = false;
     player.activate();
     startAnimating(30);
