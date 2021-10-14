471a21007b457dce47efb563bf3cad514baee1d5
MartiBrgs <martinbrgs@gmail.com>
2021-10-14 19:16:48 -0300

    created music functions to check if plays or not

--- js/audio.js
+++ js/audio.js
@@ -1,2 +1,28 @@
-// musica de fondo mundo abierto
-export const overworldMusic = new Audio("/audio/bg1.mp3");
+import { inBattle } from './battleMain.js';
+
+const townMusicSrc = new Audio("/audio/bg1.mp3"),
+    battleMusicSrc = new Audio("/audio/burning-battlefield-battle-theme.mp3"),
+    routeMusicSrc = new Audio("/audio/pokemon-anime-bgm-route-3.mp3");
+
+function routeMusic() {
+    if (!inBattle) {
+        routeMusicSrc.play();
+    } else {
+        routeMusicSrc.pause();
+        routeMusicSrc.currentTime = 0;
+    }
+}
+
+function battleMusic() {
+    if (inBattle) {
+        battleMusicSrc.play();
+    } else {
+        battleMusicSrc.pause();
+        battleMusicSrc.currentTime = 0;
+    }
+}
+
+export default {
+    battleMusic,
+    routeMusic
+}
