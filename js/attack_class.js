31419d43dc97a8c9a3b6fdf1895812af1c61756c
MartiBrgs <martinbrgs@gmail.com>
2021-10-14 18:05:09 -0300

    added attack

--- js/attack_class.js
+++ js/attack_class.js
@@ -16,11 +16,13 @@ const PHYSICAL = "Physical",
 let scratch = new Attack("Scratch", "normal", PHYSICAL, 35, 5, 100),
     tackle = new Attack("Tackle", "normal", PHYSICAL, 35, 10, 100),
     slam = new Attack("Slam", "normal", PHYSICAL, 20, 20, 75),
-    fire_punch = new Attack("Fire Punch", "fire", PHYSICAL, 15, 15, 100);
+    fire_punch = new Attack("Fire Punch", "fire", PHYSICAL, 15, 15, 100),
+    razor_leaf = new Attack("Razor Leaf", "grass", PHYSICAL, 25, 15, 95);
 
 export default {
     scratch,
     tackle,
     slam,
-    fire_punch
+    fire_punch,
+    razor_leaf
 };
